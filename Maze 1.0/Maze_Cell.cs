6e0f7c46ff47de31493a5f24756ed6fa351b038e
Oleksandr203 <octa@meta.ua>
2023-05-10 19:21:54 +0300

    Add project files.

---
+++ Maze 1.0/Maze_Cell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Maze_1._0
+{
+    internal class Maze_Cell
+    {
+        public bool CanDown {get;}
+        public bool CanUp { get;}
+        public bool CanLeft { get;}
+        public bool CanRight { get;}
+        public bool IsBridge { get; }
+        public int StartOrFinshCell { get; }
+
+        public Maze_Cell(int rowX, int columnY, bool canUp, bool canRight, bool canDown, bool canLeft, bool isBridge, int startOrFinishCell) 
+        {
+        Point point = new Point(rowX, columnY);
+            CanUp = canUp;
+            CanDown = canDown;
+            CanLeft = canLeft;
+            CanRight = canRight;
+            StartOrFinshCell = startOrFinishCell;
+        }
+    }
+}
